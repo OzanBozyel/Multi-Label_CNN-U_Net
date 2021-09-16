e07736ecf7faff141b4737b57c3c9f2a32386d68
OzanBozyel <ozanbozyel_lis@hotmail.com>
2021-09-17 01:04:01 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 09:53:43 2021
+
+@author: Ozan
+"""
+H5_FILE_PATH_UNET = 'C:/Users/Ozan/Desktop/UNET_15_EPOCHS.h5'
+
+DICE_SMOOTH = 1
+LEARNING_RATE = 0.0001
+PRINT =True
+FEATURES_MAP=16
+USE_DROPOUT = True
+DROPOUT_RATE = 0.1
+
+IMG_HEIGHT=128
+IMG_WIDTH=128
+IMG_CHANNELS=1
+n_classes=6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
