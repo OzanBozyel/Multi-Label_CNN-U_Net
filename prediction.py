5b98d826b7409827cf0b88f89101f9fa3be11a3d
OzanBozyel <ozanbozyel_lis@hotmail.com>
2021-09-17 01:03:55 +0300

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 18:27:20 2021
+
+@author: Ozan
+"""
+import settings
+from model import MODEL
+from keras.models import load_model
+import numpy as np
+
+class Prediction:
+    def __init__(self,images,
+                 h5_file_path = settings.H5_FILE_PATH_UNET):
+        self.images = images
+        self.h5_file_path = h5_file_path
+
+    def Predict(self):
+        prediction_all_images = []
+        pre_model = MODEL().U_NET()
+        pre_model = load_model(self.h5_file_path)
+        for i in range(len(self.images.shape[0])):
+            img_prediction = pre_model.predict(self.images[i])
+            predicted_img = np.argmax(img_prediction,axis=3)[0,:,:]
+            prediction_all_images.append(predicted_img)
+        prediction_all_images = np.array(prediction_all_images)
+            
+        return prediction_all_images 
+        
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
