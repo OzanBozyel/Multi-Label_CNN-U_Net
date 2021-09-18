1dd305cb6430293e3f014897a16e0737cc41e11b
OzanBozyel <ozanbozyel_lis@hotmail.com>
2021-09-19 01:51:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ Created on Mon Sep 13 20:13:03 2021
 """
 from processor import procedure
 import model
-from prediction import Prediction
 
 
 
 
-a = procedure(path ='C:/Users/Ozan/Desktop/ID_001/')
+
+a = procedure(path ='C:/Users/Ozan/Desktop/ID_001/').general_processor()
  
-b = a.general_processor()
+
 
 
 
